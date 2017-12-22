917a9ce17a3c5224ec105ce62acc1da855dc7c95
Peter Miles <hello@petermiles.io>
2017-12-22 12:27:41 -0600

    cmon....

--- src/components/connect/editLinks/AddLink/slides/LinkSlide.js
+++ src/components/connect/editLinks/AddLink/slides/LinkSlide.js
@@ -2,12 +2,35 @@ import React from "react";
 
 import { Slide, ModalHeader, ModalHeaderText } from "../styles";
 
-export default function LinkSlide(props) {
-	return (
-		<Slide>
-			<ModalHeader color={props.selected.toLowerCase()}>
-				<ModalHeaderText> {props.selected} </ModalHeaderText>
-			</ModalHeader>
-		</Slide>
-	);
+import { TextField } from "react-native-material-textfield";
+
+export default class LinkSlide extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			link: "",
+		};
+	}
+
+	render() {
+		return (
+			<Slide>
+				<ModalHeader color={this.props.selected.toLowerCase()}>
+					<ModalHeaderText> {this.props.selected} </ModalHeaderText>
+					<TextField
+						label=""
+						baseColor="black"
+						tintColor="black"
+						textColor="black"
+						containerStyle={{ width: "100%" }}
+						value={this.state.link}
+						onChangeText={link => {
+							this.setState({ link });
+						}}
+					/>
+				</ModalHeader>
+			</Slide>
+		);
+	}
 }
